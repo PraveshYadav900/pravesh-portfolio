edfa879fabeeef5458bddc175cf3b2a9ef0adbac
Pravesh Yadav <praveshyadav.jnu@gmail.com>
2025-05-25 19:36:37 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,12 @@ export const metadata: Metadata = {
   generator: 'v0.dev',
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <base href="/pravesh-portfolio/" />
+      </head>
       <body>{children}</body>
     </html>
   )
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +11,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  output: 'export',
+
+  basePath: isProd ? '/pravesh-portfolio' : '',
+  assetPrefix: isProd ? '/pravesh-portfolio/' : '',
 }
 
-export default nextConfig
+export default nextConfig;
